e3f584bd6725dff9af6f90ec984f6158f779c0e5
zakibacks <165463445+zakibacks@users.noreply.github.com>
2024-04-05 14:21:25 +0900

    修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,6 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
+    @room = Room.find(params[:room_id])
   end
 end
--- db/migrate/20240405032305_create_messages.rb
+++ db/migrate/20240405032305_create_messages.rb
@@ -1,4 +1,4 @@
-class CreateMessages < ActiveRecord::Migration[7.0]
+class CreateMessages < ActiveRecord::Migration[6.0]
   def change
     create_table :messages do |t|
       t.string  :content
